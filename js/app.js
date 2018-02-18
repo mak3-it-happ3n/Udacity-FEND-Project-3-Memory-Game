1072b7be847caf17e769daf9a723457281791364
Christoph Pätzold <ogd998@gmail.com>
2018-02-18 17:29:00 +0100

    general updates

--- js/app.js
+++ js/app.js
@@ -1,41 +1,42 @@
 
  // Create a list that holds all of the cards
 const nodeList = document.querySelectorAll('.card');
-let list1 = Array.from(nodeList); //turns that list into an array
+let list = Array.from(nodeList); //turns that list into an array
 
 // Shuffle the list of cards:
-list = shuffleOwn(list1);
+ list = shuffleOwn(list);
 
 let count = 0; // counting moves
 const deck = document.querySelector(".deck");
-
 document.querySelector('.timer').innerHTML = 0;
-
 let openCards = []; //list of revealed cards
 let rightCards = [];
 let wrongCards = [];
-let cardsLeft = 16;
-let time = 0;
-let stars = 3;
+let cardsLeft = 16; //list of unresolved cards
+let time = 0; //seconds played since first click
+let timerStatus = "off";
+let stars = 3; //current star-rating
 
-// note: no duplicated items in list up to this point (uncomment Testfunction below)
+// note: no duplicated items in list up to this point (uncomment Testfunction below to confirm)
+
 //Testfunction:
 
 for (let i = 0; i < list.length; i++) {
   console.log("index " + i +": " + list[i].innerHTML);
 };
-
 console.log(list[0].innerHTML);
 console.log(list[0].innerHTML);
 
-// note: strange behaviour: testfunction logs items in wrong order: even though
-// for-loop should list items starting with list[0].innerHTML. But after logging,
-// list[0].innerHTML in the CONSOLE it does NOT match the first item from the for-loop.
+/*
+note: strange behaviour: testfunction logs items in wrong order: even though
+for-loop should list items starting with list[0].innerHTML. But after logging,
+list[0].innerHTML manually in the CONSOLE it does NOT match the first item from the for-loop! why?
+*/
 
 /* Display shuffled cards
 (note: there is a problem in the function definiton */
 
-// shuffledDeck();
+//shuffledDeck();
 
 /*
 note: now there are duplicated items in the list (even though the list was not
@@ -65,7 +66,9 @@ function reset(){
   count = 0;
   cardsLeft = 16;
   stars = 3;
+  timerStatus = "off"
   time = 0;
+  document.querySelector('.timer').innerHTML = time;
   let rating = document.querySelector('.stars');  // resetting the stars
   rating.children[0].innerHTML = '<i class="fa fa-star"></i>';
   rating.children[1].innerHTML = '<i class="fa fa-star"></i>';
@@ -73,12 +76,12 @@ function reset(){
 }
 
 // only for testing the shuffleOwn-function:
-
+/*
 let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(testArray);
 testArray = shuffleOwn(testArray);
 console.log(testArray); // works fine without any duplicates!
-
+*/
 
 // @description my own shuffle function:
 function shuffleOwn(arr) {
@@ -107,22 +110,6 @@ function shuffle(array) {
 }
 */
 
-function countSeconds(){
-  setInterval(function(){
-    document.querySelector('.timer').innerHTML += 1;
-  }, 1000);
-
-  //document.querySelector('.timer').innerHTML = time;
-
-  /*
-  let seconds = document.querySelector('.timer').innerHTML;
-  seconds = time;
-  */
-  // setInterval(function(){seconds = time ; }, 1000);
-}
-
-// document.querySelector('.timer').innerHTML = time; // works
-
 // @description display shuffled cards in new oder:
 function shuffledDeck() {
   /* --->   THIS FUNCTION DOES NOT WORK CORRECTLY  <---
@@ -139,7 +126,10 @@ innerHTML and leave 'list' completely unchanged... */
 
  //event listener for a card
 deck.addEventListener('click', function(event){
-  timer();
+  if (timerStatus === "off") {
+    timerStatus = "on";
+    timer();
+  }
   revealCard(event.target);
   addCard(event.target);
 })
@@ -170,7 +160,6 @@ function addCard(c){
         }
       }
       counter();
-
   }
 
 //@description perform if 2 cards are the same:
@@ -215,16 +204,21 @@ refresh.addEventListener('click', function(){
   reset();
 });
 
-// @description display alert message when all cards are resolved
-// better: open new window with congrats & button to start again!
+//@description checking how many cards are left; if none: message
 function remainingDeck(){
   cardsLeft-=2;
   if (cardsLeft === 0) {
-    setTimeout(function(){
-      alert(`Congrats! You won the game with ${count} moves in ${time} seconds!
-        That's a ${stars}-star rating!
-        Do you want to play again?`);
+    alert(`Congrats! You won the game with ${count} moves in ${time} seconds!
+    That's a ${stars}-star rating!
+    Do you want to play again?`);
+    reset();
+    /*
+    setTimeout(function(message){
+      let myWindow = window.open("congrats.html", "myWindow", "width=350, height=250");
+      myWindow.document.write("<p>This is 'myWindow'</p>");
     }, 750);
+     myWindow.document.write("<p>This is 'myWindow'</p>");
+     */
   }
 }
 
@@ -243,13 +237,21 @@ function starRating1(){
 }
 
 //@description start timer if it's not already running
-function timer(){
-  if (time === 0) {
-    setInterval(function(){   // sets time
-      time += 1;
-    }, 1000);
-    setInterval(function(){
-      document.querySelector('.timer').innerHTML = time;
-    }, 1000);
-  }
+function timer() {
+    if (time === 0) {
+      setInterval(function(){   // sets time
+        if (timerStatus === "on") {
+          time += 1;
+        } else {
+          return;
+        }
+      }, 1000);
+      setInterval(function(){
+        if (timerStatus === "on") {
+          document.querySelector('.timer').innerHTML = time;
+        } else {
+          return;
+        }
+      }, 1000);
+    }
 }
---
+++ js/congrats.js
@@ -0,0 +1,8 @@
+let test = window.opener.stars;
+ocument.querySelector('.congratMessage').innerHTML = test;
+
+
+/*
+document.querySelector('.congratMessage').innerHTML = myVariable;
+// document.querySelector('.congratMessage').innerHTML = message;
+*/
